54fad5244a30488c43e718f9c1b22538883464b4
Preetham <preethamj922@gmail.com>
2026-06-17 15:41:10 +0530

    Fix Vercel deployment

--- app.py
+++ app.py
@@ -311,4 +311,4 @@ def handle_disconnect():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, use_reloader=False)
